f3c994c010647e96c0d2ef9daeeb691089e732c3
root <root@DESKTOP-O1LD4B7.localdomain>
2021-11-24 15:10:08 +0100

    chui con

---
+++ serviceWorker.js
@@ -0,0 +1,50 @@
+const cacheName = "ginkoBusCache";
+const filesCached = [
+    'app.js',
+    'style.css',
+    'index.html',
+    'manifest.json',
+    'icons/favicon.ico',
+    'icons/icon-32.png',
+    'icons/icon-64.png',
+    'icons/icon-96.png',
+    'icons/icon-128.png',
+    'icons/icon-168.png',
+    'icons/icon-180.png',
+    'icons/icon-190.png',
+    'icons/icon-192.png',
+    'icons/icon-256.png',
+    'icons/icon-512.png',
+    'icons/maskable_icon.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(filesCached);
+    }))
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        return caches.delete(key);
+        }))
+    }));
+});
